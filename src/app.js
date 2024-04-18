6eccfd8a306a2c973ac660527a4f3459f3b97551
Ariel Fink <arielqs61@gmail.com>
2024-04-17 23:39:18 -0300

    Set Handelbars

--- src/app.js
+++ src/app.js
@@ -1,15 +1,36 @@
 import express from 'express'
 import productsRouter from './routes/products.router.js'
 import { cartsRouter } from './routes/carts.router.js'
+import { __dirname } from './utils.js'
+import handlebars from 'express-handlebars'
 
 const app = express() 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname+'/views')
+app.set('view engine', 'handlebars')
+
+
+const prods = await products.readFile()
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+
+        prods,
+        role
+
+    }) 
+
+})
+
 const main = () => {
     
     app.use('/api/products', productsRouter)
     app.use('/api/carts', cartsRouter)
+
 
     app.listen(8080, error => {
 
